057ee0ed2f2d295524a57818768a1293fbff1e47
Lorraineken <lorrainekupa@gmail.com>
2023-03-27 13:25:29 +0300

    Added validation error message

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,12 +1,19 @@
 class HeroPowersController < ApplicationController
+ rescue_from ActiveRecord::RecordInvalid, with: :invalid_hero_powers
 
   def create 
     hero_power = HeroPower.create!(hp_params)
     render json: hero_power
   end
+
+  private
  
    def hp_params
     params.permit(:strength, :hero_id, :power_id)
+   end
+
+   def invalid_hero_powers 
+    render json:{ "errors": ["validation errors"]}
    end
 
 end
