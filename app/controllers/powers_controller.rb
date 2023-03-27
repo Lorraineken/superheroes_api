7ecf9b35c475028e90b0b471f93694ee806120f2
Lorraineken <lorrainekupa@gmail.com>
2023-03-27 12:22:36 +0300

    Enabled index and show routes

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,13 @@
 class PowersController < ApplicationController
+
+  def index 
+    power = Power.all 
+    render json: power
+  end
+
+  def show 
+    power = Power.find(params[:id])
+    render json: power
+  end
+
 end
