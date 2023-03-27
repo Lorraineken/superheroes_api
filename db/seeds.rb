21580027157accf704d334f265ef22187b508a2d
Lorraineken <lorrainekupa@gmail.com>
2023-03-27 11:46:40 +0300

    Generated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# create HEROES
+4.times do 
+    hero_name =Faker::Games::Heroes.name
+    hero_super_name =Faker::Superhero.name
+    Hero.create!(name:hero_name, super_name:hero_super_name)
+end
+
+#create Powers
+3.times do 
+    power_name =Faker::Superhero.power
+    power_description = Faker::Movies::StarWars.wookiee_sentence
+    Power.create!(name:power_name,description:power_description)
+end
+
+#create hero_powers
+HeroPower.create!(strength:"Strong",hero_id:1,power_id:1)
+HeroPower.create!(strength:"Weak",hero_id:1,power_id:2)
+HeroPower.create!(strength:"Average",hero_id:1,power_id:3)
+HeroPower.create!(strength:"Strong",hero_id:2,power_id:2)
+HeroPower.create!(strength:"Weak",hero_id:2,power_id:1)
+HeroPower.create!(strength:"Average",hero_id:2,power_id:3)
+HeroPower.create!(strength:"Strong",hero_id:3,power_id:3)
+HeroPower.create!(strength:"Weak",hero_id:3,power_id:2)
+HeroPower.create!(strength:"Average",hero_id:3,power_id:1)
+HeroPower.create!(strength:"Strong",hero_id:4,power_id:1)
+HeroPower.create!(strength:"Weak",hero_id:4,power_id:2)
+HeroPower.create!(strength:"Average",hero_id:4,power_id:3)
+
